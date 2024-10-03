9aae3e63f6141e525fdf5efe801f470a2a668639
AdrianKnu <knuppelholzadrian@gmail.com>
2024-10-03 18:22:17 -0300

    Manejo de TipoDatos

---
+++ TiposDatos.java
@@ -0,0 +1,22 @@
+
+import java.time.LocalDate;
+public class TiposDatos {
+    public static void main(String[] args) {
+        TiposDatos p = new TiposDatos();
+        System.err.println("Ejecutando mi primer funcion:");
+        p.Prueba();
+    }
+
+    public void Prueba(){
+        double temperatura = 231, distancia =21;
+        float precio = 34;
+        String nombre = "Dario", numTel = "3764716434";
+        LocalDate fechDate;
+        long cantHabitantes = 389897087;
+        boolean stock = true;
+        int duracion = 2, ejemplares = 5;
+        System.out.println(temperatura);
+    // System.err.println("La temperatura es: ", temperatura ,"La distancia: ", distancia, "Kilometros.");
+    }
+    
+}
